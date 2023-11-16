63c317aa15cdad3de48d568d2450a594b25a792d
Dom Holmes <Dom.carlos29@icloud.com>
2023-11-16 16:07:51 -0500

    dom-push

--- SwiftUIStoryTemplate/Chapter1/Chapter1Page3.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1Page3.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct Chapter1Page3: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //Color.gray
+                //.ignoresSafeArea()
+            Image("deepsea")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("ai no ox space")
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(
+                        Rectangle()
+                            .stroke(.cyan, style:StrokeStyle(lineWidth:12))
+                    )
+                    .navigationTitle("Breath Holding in Amsterdam")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding()
+                Text ("Once I made it to the third Q-Line train stop there was a bus in front putting out some dirty air. I thought it was a good time to challenge myself and see how long I could hold my breath. I made it to 135 seconds, and I could have gone longer but I was at the next stop where I was meeting my friends.")
+                    //.font(.title)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 12.0)
+                        Color.cyan
+                    }
+            }
+           // .background(
+                
+        }
+                
     }
 }
-
 #Preview {
     Chapter1Page3()
 }
--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -2,9 +2,52 @@ import SwiftUI
 
 struct Chapter1View: View {
     var body: some View {
-        Text("Chapter 1")
+        ZStack {
+            Color.black
+            .ignoresSafeArea()
+            VStack {
+                Text(" THE Q-LINE EXP ")
+                    .bold()
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundStyle(.red)
+                NavigationStack {
+                    
+                    List {
+                        Image("ai qline")
+                            .resizable()
+                            .scaledToFit()
+                        //.frame(height: 300.0)
+                            .overlay(
+                                Rectangle()
+                                    .stroke(.black, style:StrokeStyle(lineWidth:8))
+                            )
+                        Section("Train Stops    🚇")
+                        {
+                            NavigationLink(destination: Chapter1Page1()) {
+                                Label("Grand Blvd", systemImage: "building.2")
+                                
+                            }
+                            NavigationLink(destination: Chapter1Page2()) {
+                                Label("Baltimore", systemImage: "play.circle")
+                            }
+                            NavigationLink(destination: Chapter1Page3()) {
+                                Label("Amsterdam", systemImage:"water.waves")
+                            }
+                        }
+                    }
+                    .background(.red)
+                    .scrollContentBackground(.hidden)
+                    //.foregroundColor(.red)
+                }
+                
+            }
+        }
+        .navigationTitle ("THE Q-LINE EXP")
+        .font(.headline)
     }
 }
+          
+            
 
 #Preview {
     Chapter1View()
