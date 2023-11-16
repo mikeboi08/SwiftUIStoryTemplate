8f1daf7938bd1fe1894eb5ddd9be604b069e0988
Dom Holmes <Dom.carlos29@icloud.com>
2023-11-16 15:55:57 -0500

    created whole chapter

---
+++ SwiftUIStoryTemplate/Chapter1/Chapter1Page2.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter1Page2.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Dominique C Holmes on 11/16/23.
+//
+
+import SwiftUI
+
+struct Chapter1Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chapter1Page2()
+}
---
+++ SwiftUIStoryTemplate/Chapter1/Chapter1Page3.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter1Page3.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Dominique C Holmes on 11/16/23.
+//
+
+import SwiftUI
+
+struct Chapter1Page3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chapter1Page3()
+}
